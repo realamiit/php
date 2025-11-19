e1ff84cee1d0bc6a92e67e247464b1cb3cd24941
Amit Gupta <your@email.com>
2025-11-20 00:20:36 +0530

    start php

---
+++ test.php/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+         This is my first pHp webpage; <br>
+         <?php
+            echo "Hello World and this is my first php programm!";
+
+
+            ?>
+            This is my first pHp webpage; <br>
+         <?php
+            echo "Hello World again!";
+
+        
+
+
+            ?>
+
+This is my first pHp webpage; <br>
+         <?php
+            echo "Hello WorldAlso again!";
+
+            
+
+
+            ?>            </div>
+</body>
+</html>
