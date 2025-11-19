2979a99666f3365d6443a02168df333e4c680350
Amit Gupta <your@email.com>
2025-11-20 01:06:12 +0530

    all operator in this slide

--- test.php/index.php
+++ test.php/index.php
@@ -16,17 +16,96 @@
             comment 
             
             */
+//             ?>
+//             This is my first pHp webpage; <br>
+// This is my first pHp webpage; <br>
+//          <?php
+               echo "Hello WorldAlso again!";
+  
+               $variable1 = 5;
+               $variable2 = 6;
+               echo $variable1 + $variable2
+               operators in pHp
+               Arithmetic operators  
+               + - * / %
+               echo "The value of the variable1 - variables2 is:";
+               echo $variable1 - $variable2; <br>
+               echo "The value of the variable1 + variables2 is:";
+               echo $variable1 + $variable2; <br>
+               echo "The value of the variable1 * variables2 is:";
+               echo $variable1 * $variable2; <br>
+               echo "The value of the variable1 / variables2 is:";
+               echo $variable1 / $variable2; <br>
+               echo "The value of the variable1 % variables2 is:";
+               echo $variable1 % $variable2; <br>
+            //Assignment operators
+            // = += -= *= /= %=
+            $newVar = 10;
+
+
+            echo "The value of the variable1 after += is:";
+            echo $newVar += 1; <br>  
+            echo "The value of the variable1 after -= is:";
+            echo $newVar -= 5; <br>  
+            echo "The value of the variable1 after *= is:";
+            echo $newVar *= 5; <br>  
+            echo "The value of the variable1 after /= is:";
+            echo $newVar /= 5;  <br>
+            echo "The value of the variable1 after %= is:";
+            echo $newVar %= 5;   <br> //newVar = newVar % 5
+
+            //Comparison operators
+            // == === != <> !== > < >= <=
+
+            echo "The value of 1==6 is:";
+            echo var_dump(1==6); <br>   //var_dump shows boolean value
+            echo "The value of 1!=6 is:";
+            echo var_dump(1!=6); <br>   //var_dump shows boolean value
+            echo "The value of 1<>6 is:";
+            echo var_dump(1<>6); <br>  //var_dump shows boolean value
+            echo "The value of 1<6 is:";
+            echo var_dump(1<6); <br>  //var_dump shows boolean value
+            echo "The value of 1>6 is:";
+            echo var_dump(1>6); <br>  //var_dump shows boolean value
+            echo "The value of 1<=6 is:";
+            echo var_dump(1<=6); <br>  //var_dump shows boolean value
+            echo "The value of 1>=6 is:";
+            echo var_dump(1>=6); <br>  //var_dump shows boolean value
+            echo "The value of 1===6 is:";
+            echo var_dump(1===6); <br>   //var_dump shows boolean value
+            echo "The value of 1!==6 is:";
+            echo var_dump(1!==6); <br> //Bitwise operators
+            
+            //Increment/Decrement operators
+            // ++ -- --
+            echo "The value of newVar is:";
+            echo $newVar; <br>
+            echo "The value of newVar after ++ is:";    //post-increment
+            echo $newVar++; <br>  //post-increment
+            echo "The value of newVar after ++ is:";  //pre-increment
+            echo ++$newVar; <br>  //pre-increment
+            echo "The value of newVar after -- is:";    //post-decrement
+            echo $newVar--; <br>  //post-decrement    //decreases value but shows previous value
+            echo "The value of newVar after -- is:";   //pre-decrement
+            echo --$newVar; <br>  //pre-decrement
+            //Logical operators
+            echo "The value of true and false is:";  //and
+            echo var_dump(true and false); <br>  //and
+            echo "The value of true or false is:";
+            echo var_dump(true or false); <br>   //or
+            echo "The value of true xor false is:";
+            echo var_dump(true xor false); <br>   //xor
+            echo "The value of !true is:";
+            echo var_dump(!true); <br>   //not
+            echo "The value of true && false is:";
+            echo var_dump(true && false); <br>   //&&
+            echo "The value of true || false is:";
+            echo var_dump(true || false); <br>   //||
+            // and or xor && || !
+
+
+
             ?>
-            This is my first pHp webpage; <br>
-This is my first pHp webpage; <br>
-         <?php
-            echo "Hello WorldAlso again!";
-
-            $variable = 33;
-            $variable2=44;
-            echo $variable + $variable2;
-
-
-            ?>            </div>
+        </div>
 </body>
 </html>
